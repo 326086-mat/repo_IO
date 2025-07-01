8e08c1c73553b1939556e21957c8da7b12870069
Michal <thepolishbear156@mat.umk.pl>
2025-07-01 10:55:58 +0200

    modified:   projekt1.cpp

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
+bool isNumberPrime(int a)
+{
+	if(a <= 1)
+	{
+		return false;
+	}
+
+	if(a == 2) 
+	{
+		return true;
+	}
+
+	if(a % 2 == 0)
+	{
+		return false;
+	}
+
+	for(int i = 3; i * i <= a; i += 2)
+	{
+		if(a % i == 0)
+		{
+			return false;
+		}
+	}
+
+return true;
+}
+
 int main() 
 {
 	int a, b;
